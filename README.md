ae18ff462cd159bee6f7bc2fe5182c5f988cdfe5
ongebo <isaacongebo@gmail.com>
2021-03-15 00:40:09 +0300

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FindIt Offline First
+
+A Simple Project to Manage Lost and Found Items, Features an Offline-first Approach.
---
+++ android/app/src/main/kotlin/com/ongebo/findit_offline_first/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ongebo.findit_offline_first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
